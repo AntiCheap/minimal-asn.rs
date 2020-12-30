a98ef71abe620a777092e7e90dc2acf20af657dc
AntiCheap <stefanofalcetta@gmail.com>
2020-12-30 19:32:04 +0100

    Update asn.rs

--- asn.rs
+++ asn.rs
@@ -1,53 +1,39 @@
-pub fn split(data: &[u8]) -> Option<Vec<&[u8]>> {
-    let mut i = 1;
-    if let Some(_) = prefixed(data, &mut i) {
-        let mut res = vec![];
-        while i < data.len() {
-            if data[i] == 2 {
-                i += 1;
-                if let Some(n) = prefixed(data, &mut i) {
-                    res.push(&data[i..i + n]);
-                    i += n;
-                    continue;
-                }
-            }
-            return None;
-        }
-        return Some(res);
-    }
-    return None;
-}
 
-fn prefixed(data: &[u8], i: &mut usize) -> Option<usize> {
-    let mut res = None;
-    if *i < data.len() {
-        if data[*i] <= 128 {
-            let val = data[*i] as usize;
-            *i += 1;
-            res = Some(val);
-        } else {
-            let mut get = data[*i] as usize - 128;
-            let end = *i + get + 1;
-            if data.len() > *i + get {
-                let mut sum = 0;
-                let mut mul = 1;
-                while get > 0 {
-                    sum += data[*i + get] as usize * mul;
-                    if sum > 16000 {
-                        return None;
-                    }
-                    mul *= 256;
-                    get -= 1;
+mod asn {
+    #[derive(Debug)]
+    pub struct AsnData<'a>(pub u8, pub &'a [u8]);
+
+    pub fn data_split(mut data: &[u8]) -> Option<Vec<AsnData>> {
+        let mut res = vec![];
+        while let Some(id) = data.get(0) {
+            let (packet, rest) = prefixed(&data[1..])?;
+            res.push(AsnData(*id, packet));
+            data = rest;
+        }
+        Some(res)
+    }
+
+    pub fn prefixed(data: &[u8]) -> Option<(&[u8], &[u8])> {
+        //One byte 0..127, many bytes = N & 0x7F
+        data.split_first().and_then(|(one, rest)| {
+            let one = *one as usize;
+            let (len, res) = if one < 128 {
+                (0, one)
+            } else {
+                let len = one & 0x7f;
+                if len > 4 || len > rest.len() {
+                    return None;
                 }
-                *i = end;
-                res = Some(sum);
-            }
-        }
+                let mut res = 0;
+                for x in 0..len {
+                    res = res << 8 | rest[x] as u32;
+                }
+                (len, res as usize)
+            };
+            let tot = len + res;
+            let next = rest.get(tot..)?;
+            let this = rest.get(len..tot)?;
+            Some((this, next))
+        })
     }
-    if let Some(x) = res {
-        if *i + x <= data.len() {
-            return Some(x);
-        }
-    }
-    return None;
 }
